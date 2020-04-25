971e4495b7767867b546bfcb05ee35da5e82740e
chehuan <18271645764@163.com>
2020-04-25 23:51:14 +0800

    数据结构

---
+++ docs/dataStructures-algorithms/Structures/数组结构/Tree/BST/红黑树/RedBlackTree.java
@@ -0,0 +1,18 @@
+package Tree.BST.红黑树;
+
+/**
+ * 红黑树 也是一种二叉搜索树
+ * 性质:
+ *      节点必须满足5个性质:
+ *          节点是red或者black
+ *          根节点是black
+ *          叶子节点(外部节点,空节点)都是黑色
+ *          假想为真二叉树,度为2
+ *          red节点都是子节点都是black,的父节点是black
+ *          从任一节点到叶子节点都包括相同数目的black节点
+ *
+ *
+ */
+public class RedBlackTree {
+
+}
